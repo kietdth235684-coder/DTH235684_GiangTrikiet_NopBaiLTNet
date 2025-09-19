7d0a2042a4de69d907754961d979576794e225bd
kietdth235684-coder <kiet_dth235684@student.agu.edu.vn>
2025-09-19 15:30:34 +0700

    BuoiTH3

---
+++ Buoithuchanh3/Buoi3_bai1/Form1.cs
@@ -0,0 +1,59 @@
+﻿namespace Buoi3_bai1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string thongbao;
+            thongbao = "Tên đăng nhập là: ";
+            thongbao += this.txtUser.Text;
+            thongbao += "\n\rMật khẩu là:";
+            thongbao += this.txtPass.Text;
+            if (this.chkNho.Checked == true)
+                thongbao += "\n\rBạn có ghi nhớ.";
+            MessageBox.Show(thongbao, "Thông báo");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.txtUser.ResetText();
+            this.txtPass.ResetText();
+            this.txtUser.Focus();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult traloi;
+            traloi = MessageBox.Show("Bạn có muốn đóng chương trình không?", "Thông báo",
+            MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (traloi == DialogResult.OK)
+                Application.Exit();
+        }
+    }
+}
---
+++ Buoithuchanh3/Buoi3_bai2/Form1.cs
@@ -0,0 +1,10 @@
+namespace Buoi3_bai2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
